f1619e8aa9232321ec7b1763c357044684e9b014
Cutter72 <drelich_pawel@o2.pl>
2019-02-05 20:11:52 +0100

    Author Entity created

---
+++ src/main/java/pl/domowe/domain/Author.java
@@ -0,0 +1,18 @@
+package pl.domowe.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+}
--- src/main/java/pl/domowe/domain/Category.java
+++ src/main/java/pl/domowe/domain/Category.java
@@ -1,8 +1,11 @@
 package pl.domowe.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
+@Data
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
