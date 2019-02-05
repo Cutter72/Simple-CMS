6eee5948f133886084e5084ec3be743f21b00a8d
Cutter72 <drelich_pawel@o2.pl>
2019-02-05 23:38:15 +0100

    Imports cleaned

--- src/main/java/pl/domowe/controller/HomePageController.java
+++ src/main/java/pl/domowe/controller/HomePageController.java
@@ -1,9 +1,7 @@
 package pl.domowe.controller;
 
-import org.hibernate.Hibernate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.domowe.dao.ArticleDao;
 import pl.domowe.dao.AuthorDao;
